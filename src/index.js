ba6a8c8dcbbe8abc252aa91e2c35ae74ba2eec1f
lvheng <1834805770@qq.com>
2021-01-02 22:23:35 +0800

    添加拷贝多种类型的对象

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 function deepClone(source) {
+  if (source instanceof Object) {
+    if (source instanceof Array) {
+      const dist = new Array()
+      for (let key in source) {
+        dist[key] = deepClone(source[key])
+      }
+      return dist
+    } else if (source instanceof Function) {
+      const dist = function() {
+        return source.apply(this, arguments)
+      }
+      for (let key in source) {
+        dist[key] = deepClone(source[key])
+      }
+      return dist
+    } else {
+      const dist = new Object()
+      for (let key in source) {
+        dist[key] = deepClone(source[key])
+      }
+      return dist
+    }
+  }
   return source
 }
 
--- test/index.js
+++ test/index.js
@@ -36,4 +36,35 @@ describe('deepClone', () => {
     const sym2 = sym
     assert(sym === sym2)
   })
+  describe('# 对象', () => {
+    it('能够复制基本的对象', () => {
+      const a = { name: '张三', child: { name: '小李四' } }
+      const a2 = deepClone(a)
+      assert(a !== a2)
+      assert(a.name === a2.name)
+      assert(a.child !== a2.child)
+      assert(a.child.name === a2.child.name)
+    })
+    it('能够复制数组', () => {
+      const a = [[11, 12], [21, 22], [31, 32]]
+      const a2 = deepClone(a)
+      assert(a !== a2)
+      assert(a[0] !== a2[0])
+      assert(a[1] !== a2[1])
+      assert(a[2] !== a2[2])
+      // 对比两个数组所有的子属性
+      assert.deepEqual(a, a2)
+    })
+    it('能够复制函数', () => {
+      const a = function(x, y) { return x + y }
+      a.k1 = { k2: { k3: 1} }
+      const a2 = deepClone(a)
+      assert(a !== a2)
+      assert(a.k1 !== a2.k1)
+      assert(a.k1.k2 !== a2.k1.k2)
+      assert(a.k1.k2.k3 === a2.k1.k2.k3)
+      assert(a(1, 2) === a2(1, 2))
+    })
+  })
+  
 })
